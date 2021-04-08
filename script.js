5a5842fbfaed30b2c67146aa5a390b7d58b35f1d
Markki Meyer <markkimeyer@gmail.com>
2021-04-08 13:16:12 -0500

    Initial push from previous repo

---
+++ script.js
@@ -0,0 +1,70 @@
+$(document).ready( function() {
+
+
+    
+
+
+
+
+
+
+//On click projectone live page
+
+$(document).on("click", "#project-one-live", function () {
+    window.open("https://markkimeyer.github.io/RechargeCarApp/");
+})
+
+//on hover
+// $(document).on("hover", "#project-one-live", function () {
+//     $text(Live Github Page);
+// })
+
+//On click projectone repo 
+
+$(document).on("click", "#project-one-repo", function () {
+    window.open("https://github.com/markkimeyer/RechargeCarApp");
+})
+
+//On click projecttwolive page
+
+$(document).on("click", "#project-two-live", function () {
+    window.open("https://markkimeyer.github.io/daily-planner/");
+})
+
+//On click projecttwo repo 
+
+$(document).on("click", "#project-two-repo", function () {
+    window.open("https://github.com/markkimeyer/daily-planner");
+})
+//On click project 3 live page
+$(document).on("click", "#project-three-live", function () {
+    window.open("https://markkimeyer.github.io/codeQUIZ/");
+})
+
+//On click project3 repo 
+
+$(document).on("click", "#project-three-repo", function () {
+    window.open("https://github.com/markkimeyer/codeQUIZ");
+})
+
+//email pop up form
+$(document).on("click", "#mailchimp", function () {
+    window.open("https://us1.list-manage.com/contact-form?u=ab348f2b61785522dbd6bdd88&form_id=73c9c3b608e09594c381e853f8e5ce8d");
+})
+
+//resume link to google doc  "resume"
+$(document).on("click", "#resume", function () {
+    window.open("https://drive.google.com/file/d/1V8nwHXaHcTvJLf5I8pivfCqoQdf-blCp/view?usp=sharing");
+})
+//linked in link 
+$(document).on("click", "#linkedin", function () {
+    window.open("https://www.linkedin.com/in/markkimeyer/");
+})
+
+//github link
+$(document).on("click", "#github", function () {
+    window.open("https://github.com/markkimeyer");
+})
+
+//don't delete closing tab
+})
